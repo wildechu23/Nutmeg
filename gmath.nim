eff516fbcdb431d25e9c7583082e3c98cdac96d2
Wilde Chu <wildechu23@gmail.com>
2022-05-04 11:22:01 -0400

    some setup

--- gmath.nim
+++ gmath.nim
@@ -1,4 +1,4 @@
-import matrix
+import display, matrix
 
 proc normalize(v: tuple) =
     return
@@ -14,4 +14,15 @@ proc calculateNormal*(m: Matrix, i: int): tuple =
         a: tuple = (p1[0]-p0[0], p1[1]-p0[1], p1[2]-p0[2])
         b: tuple = (p2[0]-p0[0], p2[1]-p0[1], p2[2]-p0[2])
     (a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0])
-    
+
+proc calculateAmbient(alight: Color, areflect: tuple): Color =
+    discard
+
+proc calculateDiffuse(light: Matrix, dreflect, normal: tuple): Color =
+    discard
+
+proc calculateSpecular() =
+    discard
+
+proc getLighting(normal, view: tuple, alight: Color, light: Matrix, areflect, dreflect, sreflect: tuple): Color =
+    discard
