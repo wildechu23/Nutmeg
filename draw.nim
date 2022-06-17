5f0593d1bcafa7a39ff5aba238382b663e750e4e
Wilde <wildechu23@gmail.com>
2022-06-17 07:52:08 -0400

    sigh

--- draw.nim
+++ draw.nim
@@ -1086,7 +1086,7 @@ proc drawPPolygons*(m, n: var Matrix, s: var Screen, zb: var ZBuffer, view: tupl
             pScanLine(m, n, i, s, zb, view, light, ambient, areflect, dreflect, sreflect)
 
 proc drawPPolygons*(m, n: var Matrix, t: seq[(string, float, float)], symTab: seq[SymTab], s: var Screen, zb: var ZBuffer, view: tuple, light: Matrix, ambient: Color, areflect, dreflect, sreflect: tuple) =
-    echo n[0]
+    # echo n[0]
     for i in 0..<(m.len div 3):
         let fnormal = ((n[3*i][0] + n[3*i+1][0] + n[3*i+2][0]) / 3, (n[3*i][1] + n[3*i+1][1] + n[3*i+2][1]) / 3, (n[3*i][2] + n[3*i+1][2] + n[3*i+2][2]) / 3)
         # let norm = calculateNormal(m, 3*i)
